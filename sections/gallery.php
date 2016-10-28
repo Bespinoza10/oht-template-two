0123847f8e4c00b334c90d12ffa74c60a859be52
Brandon Espinoza <espinozab100@gmail.com>
2016-10-27 22:37:33 -0500

    Added new images to gallery and created css hover on images with transitions

--- sections/gallery.php
+++ sections/gallery.php
@@ -1,16 +1,44 @@
-<div id="gallery" class="container-fluid">
+<div id="gallery" class="gallery-container container-fluid">
   <div class="grid">
-    <div class="grid-item gallery-img col-md-6">
+
+    <div class="grid-item caption gallery-img col-md-6">
       <img class="img-responsive" src="img/g1.jpg" alt="" />
+      <div class="caption-overlay">
+        <h2 class="after">Custom Cleats</h2>
+      </div>
     </div>
-    <div class="grid-item gallery-img grid-item--width2 col-md-6">
+
+
+    <div class="grid-item caption gallery-img col-md-6">
       <img class="img-responsive" src="img/g3.jpg" alt="" />
+      <div class="caption-overlay">
+        <h2 class="after">Custom Cleats</h2>
+      </div>
     </div>
-    <div class="grid-item gallery-img col-md-6">
+    <div class="grid-item caption gallery-img col-md-6">
       <img class="img-responsive" src="img/g2.jpg" alt="" />
+      <div class="caption-overlay">
+        <h2 class="after">Custom Cleats</h2>
+      </div>
     </div>
-    <div class="grid-item gallery-img col-md-8">
+
+    <div class="grid-item caption gallery-img col-md-6">
       <img class="img-responsive" src="img/g4.jpg" alt="" />
+      <div class="caption-overlay">
+        <h2 class="after">Custom Cleats</h2>
+      </div>
+    </div>
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g5.jpg" alt="" />
+      <div class="caption-overlay">
+        <h2 class="after">Custom Cleats</h2>
+      </div>
+    </div>
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g6.jpg" alt="" />
+      <div class="caption-overlay">
+        <h2 class="after">Custom Cleats</h2>
+      </div>
     </div>
   </div>
 </div>
