f363a0d727d587704bdfce0e8dd40a3521cb5106
Brandon Espinoza <espinozab100@gmail.com>
2016-10-27 23:18:10 -0500

    Added nice hover on images with only custom css

--- sections/gallery.php
+++ sections/gallery.php
@@ -1,7 +1,7 @@
 <div id="gallery" class="gallery-container container-fluid">
   <div class="grid">
 
-    <div class="grid-item caption gallery-img col-md-6">
+    <!-- <div class="grid-item caption gallery-img col-md-6">
       <img class="img-responsive" src="img/g1.jpg" alt="" />
       <div class="caption-overlay">
         <h2 class="after">Custom Cleats</h2>
@@ -39,6 +39,72 @@
       <div class="caption-overlay">
         <h2 class="after">Custom Cleats</h2>
       </div>
+    </div> -->
+
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g1.jpg" alt="" />
+      <figcaption>
+        <h2 class="after">Custom Cleats</h2>
+      </figcaption>
     </div>
+
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g3.jpg" alt="" />
+      <figcaption>
+        <h2 class="after">Custom Cleats</h2>
+      </figcaption>
+    </div>
+
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g2.jpg" alt="" />
+      <figcaption>
+        <h2 class="after">Custom Cleats</h2>
+      </figcaption>
+    </div>
+
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g5.jpg" alt="" />
+      <figcaption>
+        <h2 class="after">Custom Cleats</h2>
+      </figcaption>
+    </div>
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g4.jpg" alt="" />
+      <figcaption>
+        <h2 class="after">Custom Cleats</h2>
+      </figcaption>
+    </div>
+
+    <div class="grid-item caption gallery-img col-md-6">
+      <img class="img-responsive" src="img/g6.jpg" alt="" />
+      <figcaption>
+        <h2 class="after">Custom Cleats</h2>
+      </figcaption>
+    </div>
+    <!-- <figure class="snip1206">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample74.jpg" alt="sample74"/>
+      <figcaption>
+        <h2>Jason Response</h2>
+        <p>Just makes people try to do everything at once.</p>
+      </figcaption>
+      <a href="#"></a>
+    </figure>
+    <figure class="snip1206 hover">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample69.jpg" alt="sample69"/>
+      <figcaption>
+        <h2>Fleece Marigold</h2>
+        <p>We'd invent machines that do things less efficiently.</p>
+      </figcaption>
+      <a href="#"></a>
+    </figure>
+    <figure class="snip1206">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample13.jpg" alt="sample13"/>
+      <figcaption>
+        <h2>Ruby Von Rails</h2>
+        <p>To the machines and go play outside.</p>
+      </figcaption>
+      <a href="#"></a>
+    </figure> -->
+
   </div>
 </div>
